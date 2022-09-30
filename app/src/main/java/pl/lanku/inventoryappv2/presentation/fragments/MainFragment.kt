8c746d2852759162691624afa38927af2f42480e
Kacper Kawala <kacper.jerzy.kawala@gmail.com>
2022-09-30 10:22:56 +0200

    alpha v0.0.10

--- app/src/main/java/pl/lanku/inventoryappv2/presentation/fragments/MainFragment.kt
+++ app/src/main/java/pl/lanku/inventoryappv2/presentation/fragments/MainFragment.kt
@@ -7,10 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import pl.lanku.inventoryappv2.R
 import pl.lanku.inventoryappv2.databinding.MainFragmentBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import pl.lanku.inventoryappv2.presentation.products.InventoryViewModel
 
 class MainFragment : Fragment(R.layout.main_fragment) {
-    //    private var viewModel: InventoryViewModel by viewModel()
+    private var viewModel: InventoryViewModel by viewModel()
     private lateinit var binding: MainFragmentBinding
+
 
     companion object {
         fun newInstance(): MainFragment {
@@ -31,6 +34,9 @@ class MainFragment : Fragment(R.layout.main_fragment) {
         binding = MainFragmentBinding.inflate(layoutInflater)
 
         binding.buttonProductList.setOnClickListener {
+            viewModel.ean_or_ian = binding.editTextEanOrIan.getText().toString()
+            viewModel.product_name = binding.editTextProductName.getText().toString()
+            viewModel.product_category = binding.editTextProductCategory.getText().toString()
 
         }
     }
--- app/src/main/java/pl/lanku/inventoryappv2/presentation/products/InventoryViewModel.kt
+++ app/src/main/java/pl/lanku/inventoryappv2/presentation/products/InventoryViewModel.kt
@@ -3,6 +3,12 @@ package pl.lanku.inventoryappv2.presentation.products
 import androidx.lifecycle.ViewModel
 
 class InventoryViewModel : ViewModel() {
+    lateinit var ean_or_ian : String
+    lateinit var product_name : String
+    lateinit var product_category :String
 
+    fun insertDataToDatabase(ean:String, name:String, category:String){
+
+    }
 
 }
