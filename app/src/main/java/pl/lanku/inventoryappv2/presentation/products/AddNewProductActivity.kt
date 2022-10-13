ac60f74aa1326dd0d69c2a701a157f0ba410493f
Kacper <kacper.jerzy.kawala@gmail.com>
2022-10-13 06:56:15 +0200

    alpha v0.0.19.1

---
+++ app/src/main/java/pl/lanku/inventoryappv2/presentation/products/AddNewProductActivity.kt
@@ -0,0 +1,36 @@
+package pl.lanku.inventoryappv2.presentation.products
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import pl.lanku.inventoryappv2.R
+import pl.lanku.inventoryappv2.databinding.ActivityProductsBinding
+
+class AddNewProductActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityProductsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityProductsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //error \/
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        //error /\
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
--- app/src/main/java/pl/lanku/inventoryappv2/presentation/products/ProductListFragment.kt
+++ app/src/main/java/pl/lanku/inventoryappv2/presentation/products/ProductListFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import pl.lanku.inventoryappv2.R
 import pl.lanku.inventoryappv2.databinding.FragmentProductListBinding
@@ -21,8 +20,8 @@ class ProductListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentProductListBinding.inflate(inflater,container,false)
-        return  binding.root
+        _binding = FragmentProductListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
